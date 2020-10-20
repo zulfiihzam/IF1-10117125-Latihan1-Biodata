6b3cd5bb39adcc3b9b25b3ced7f879a6a89a2dd6
zulfiihzam <35595962+zulfiihzam@users.noreply.github.com>
2020-10-21 00:18:00 +0700

    Add files via upload

---
+++ IF1-10117125-Latihan1-Biodata/src/if1/pkg10117125/latihan1/biodata/IF110117125Latihan1Biodata.java
@@ -0,0 +1,29 @@
+/*
+ * 
+ * @author
+ * NAMA : Zulfi Ihzam R
+ * KELAS : PBO1
+ * NIM : 10117125
+ *
+ */
+package if1.pkg10117125.latihan1.biodata;
+
+public class IF110117125Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "--------------------");
+        System.out.println("NAMA\t\t: Zulfi Ihzam Rahmat");
+        System.out.println("ALAMAt\t\t: Soreang");
+        System.out.println("TEMPAT LAHIR\t: Soreang");
+        System.out.println("TANGGAL LAHIR   : 04 Maret 1999");
+        System.out.println("-------------------");
+        System.out.println("HOBI\t\t: Bermain Game");
+        System.out.println("MAKES\t\t: Bakso");
+
+    }
+
+}
